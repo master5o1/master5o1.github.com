3ee2bce1f6308385047640239446fbd7ebbc7e1d
Jason <jason@master5o1.com>
2012-01-08 15:56:26 +1300

    Trying this.

---
+++ pages.js
@@ -0,0 +1,10 @@
+pages = {
+	"About" : {
+		"title" : "About",
+		"content" : "Hi, I'm Jason.<br /><br />I was born in Switzerland back in 1989, but I've lived in New Zealand since I was two.<br /><br />I enjoy hacking bits of code around in various languages. I have experience programming using PHP, JavaScript, Java, C, C++, C# and Bash.<br /><br />I like skiing and snowboarding and I've been down at Mt. Ruapehu having fun since 2003."
+	},
+	"bbPress-Post-Toolbar" : {
+		"title" : "bbPress Post Toolbar",
+		"content" : "bbPress Post Toolbar"
+	}
+}
