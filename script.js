4e2df48d53a38fef29c484bfa5bf231c26f9ee4e
Jason <jason@master5o1.com>
2012-10-26 11:46:39 +1300

    some magic happened. also it seems g+ api now has album support, if it didn't before :S so slideshow on jojo's pictures because album :)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$.gplus = function(data) {
+var GooglePlus = function(data) {
 	$(document).ready(function() {
 		$.each(data.items, function(key, value) {
 			var slide = '<li data-nav="' + key + '">';
@@ -14,6 +14,13 @@ $.gplus = function(data) {
 						video = '<span style="display: inline-block; font-size: 0.75em; margin: 0.5em 0.75em; font-style: oblique;">This is a video, view it <a href="' + value.object.attachments[0].url + '">here</a>.</span>';
 					}
 					slide += '<div class="photo" style="' + attachment + '">' + video + '</div><div class="photo-content"><p>' + value.object.content + '</p></div>';
+				} else if (value.object.attachments[0].objectType == 'album') {
+					var album = [];
+					for (var i = 0; i < value.object.attachments[0].thumbnails.length; i++) {
+						var image = value.object.attachments[0].thumbnails[i].image.url.replace(/w[0-9]+-h[0-9]+(-p)?/, 'w464-h261-p')
+						album.push('<img src="' + image + '" alt="' + i + '" />');
+					}
+					slide += '<div class="album">' + album.join("") + '</div><div class="album-content"><p>' + value.object.content + '</p></div>';
 				} else {
 					var attachment = '<h2><a href="' + value.object.attachments[0].url + '">' + value.object.attachments[0].displayName + '</a></h2>';
 					slide += attachment + '<div class="url-content"><p>' + value.object.content + '</p></div>';
@@ -26,6 +33,9 @@ $.gplus = function(data) {
 			slide += '<div class="permalink-bar"><a href="' + value.object.url + '">Permalink</a> | Updated <time class="timeago" datetime="' + value.updated + '">' + (new Date(value.updated)).toDateString() + '</time> &middot; ' + replies + ' &middot; ' + plusones + '</div>';
 			slide += '</li>';
 			$('#google-plus-slides').append(slide);
+		});
+		$('#google-plus-slides div.album').cycle({
+			timeout: 4000
 		});
 	});
 }
@@ -70,7 +80,6 @@ $(document).ready(function() {
 		outside.css('left', Math.round((width - outside.width()) / 2) + 'px');
 	}
 	
-	$.horizontal_slides = {};
 	$('#menu').css('opacity', 0).hide();
 	$('#slide > li').each(function(index){
 		var style = '';
@@ -80,14 +89,13 @@ $(document).ready(function() {
 		}
 		if ($('#slide > li').eq(index).data('menu') != false)
 			$('#menu').append('<li><a href="#/' + $('#slide > li').eq(index).data('nav') + '" class="scroller' + style + '">' + $('#slide > li').eq(index).data('nav') + '</a></li>');
-		$.horizontal_slides[$('#slide > li').eq(index).data('nav')] = index;
 	});
 
 	$('#menu').show().animate({opacity: 1.0}, 750);
 
 	$('#feature').cycle({
 		fx: 'scrollLeft',
-		timeout: 5000
+		timeout: 4000
 	});
 
 	$(window).bind('hashchange', function(event) { if (window.location.hash.substring(0,2) == '#/') $.scrolling(event); });
@@ -108,7 +116,6 @@ $.getQueryFromURL = function(url) {
 };
 
 $.submenu = function(page, slide, count) {
-	$.vertical_slides = {};
 	$('#sub-menu').css('opacity', 0).hide();
 	$('#sub-menu').html('');
 	$('li[data-nav="' + page + '"] div.slide ol > li').each(function(index){
@@ -122,7 +129,6 @@ $.submenu = function(page, slide, count) {
 		if (typeof label == 'string') { label = label.replace(/-/g,' '); }
 		else { return; label = (label + 1); }
 		$('#sub-menu').append('<li><a href="#/' + page + '/' + nav + '" class="scroller' + className + '"><span>' + label + '</span></a></li>');
-		$.vertical_slides[nav] = index;
 	});
 	if (count > 1) {
 		if (slide > 0) {
@@ -142,46 +148,63 @@ $.submenu = function(page, slide, count) {
 }
 
 $.scrolling = function(event){
-	var yx, y, x, count, hash;
+	var yx, count, hash, y_index, x_index, y_name, x_name;
 	hash = window.location.hash;
 	yx = hash.replace('#/','').split('/');
-	y = $.horizontal_slides[yx[0]];
-	count = $('#slide li[data-nav="' + yx[0] + '"] ol li').length;
+	y_index = yx[0];
+	y_name = yx[0] * 1 || yx[0];
 	if (yx.length == 1) yx[1] = 0;
-	x = yx[1] * 1 || yx[1];
-	if (typeof x == 'string') {
-		$('#slide li[data-nav="' + yx[0] + '"] .slide ol li').each(function(index) {
-			if ($('#slide li[data-nav="' + yx[0] + '"] .slide ol li').eq(index).data('nav') == yx[1]) {
-				x = index;
+	x_index = yx[1];
+	x_name = yx[1] * 1 || yx[1];
+	if (typeof y_name == 'string') {
+		$('#slide > li').each(function(index) {
+			if ($('#slide > li').eq(index).data('nav') == yx[0]) {
+				y_index = index;
 			}
 		});
-	}
-	x = x * 1;
-	if (typeof x != 'number') { x = y = 0; }
-	if ((-1 * x * $('#right').width()) == 0) {
-		$('a.creation-scroll[href="#/' + yx[0] + '"]').animate({opacity: 0.0}, 500).hide(500);
 	} else {
-		$('a.creation-scroll[href="#/' + yx[0] + '"]').show().animate({opacity: 1.0}, 500);
+		y_index = y_index * 1;
+		y_name = $('#slide > li').eq(y_index).data('nav');
 	}
-	if (x >= count-1) {
-		$('a.creation-scroll[href^="#/' + yx[0] + '/"]').animate({opacity: 0.0}, 500).hide(500);
-	} else if (x == 0) {
-		$('a.creation-scroll[href^="#/' + yx[0] + '/"]').show().animate({opacity: 1.0}, 500);
+	count = $('#slide li[data-nav="' + y_name + '"] ol li').length;
+	if (typeof x_name == 'string') {
+		$('#slide li[data-nav="' + y_name + '"] .slide ol li').each(function(index) {
+			if ($('#slide li[data-nav="' + y_name + '"] .slide ol li').eq(index).data('nav') == yx[1]) {
+				x_index = index;
+			}
+		});
+	} else {
+		x_index = x_index * 1;
+		x_name = $('#slide li[data-nav="' + y_name + '"] .slide ol li').eq(x_index).data('nav');
 	}
-	$('#slide [data-nav="' + $('body').data('current') + '"] ol').stop().animate({
-		marginLeft: 0
-	}, 400);
-	$('#slide [data-nav="' + yx[0] + '"] ol').stop().animate({
-		marginLeft: -1 * x * $('#slide').width()
+	if (typeof x_index != 'number') { x_index = y_index = 0; }
+	var current_name = "";
+	$('#slide > li ol').each(function(index) {
+		if ($('#slide > li ol').eq(index).css('marginLeft') != '0px') {
+			current_name = $('#slide > li ol').eq(index).parent().parent().data('nav');
+		}
+	});
+	if (current_name != y_name) {
+		$('#slide [data-nav="' + current_name + '"] ol').stop().animate({
+			marginLeft: 0
+		}, 400);
+	}
+	$('#slide [data-nav="' + y_name + '"] ol').stop().animate({
+		marginLeft: -1 * x_index * $('#slide').width()
 	}, 400);
 	$('#right .inside ul').stop().animate({
-		marginTop: -1 * y * $('#right').height()
+		marginTop: -1 * y_index * $('#right').height()
 	}, 200);
 	if (this != $ && this != window) {
 		event.preventDefault();
 	}
-	$('#menu [href="#/' + $('body').data('current') + '"]').css('background', 'rgba(0,0,0,0.75)');
-	$('#menu [href="#/' + yx[0] + '"]').css('background', 'rgba(0,0,0,0.95)');
-	$.submenu(yx[0], x, count);
-	$('body').data('current', yx[0]);
+	
+	$('#feature').cycle('pause');
+	$('#google-plus-slides div.album').cycle('pause');
+	if (y_name == 'Welcome') $('#feature').cycle('resume');
+	if (y_name == 'Posts') $('#google-plus-slides div.album').cycle('resume');
+	
+	$('#menu .current').removeClass('current');
+	$('#menu [href="#/' + y_name + '"]').addClass('current');
+	$.submenu(y_name, x_index, count);
 }
